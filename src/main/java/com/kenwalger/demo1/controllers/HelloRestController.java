ecb6d8509e2db493369acf829a9dc84468210976
Ken Alger <kenalger@comcast.net>
2022-11-04 08:38:44 -0700

    Added REST Controller

---
+++ src/main/java/com/kenwalger/demo1/controllers/HelloRestController.java
@@ -0,0 +1,16 @@
+package com.kenwalger.demo1.controllers;
+
+import com.kenwalger.demo1.json.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    @GetMapping("/rest")
+    public Greeting greet(@RequestParam(required = false, defaultValue = "World") String name) {
+
+        return new Greeting("Hello, " + name + "!");
+    }
+}
---
+++ src/main/java/com/kenwalger/demo1/json/Greeting.java
@@ -0,0 +1,21 @@
+package com.kenwalger.demo1.json;
+
+public class Greeting {
+    private final String message;
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+
+}
